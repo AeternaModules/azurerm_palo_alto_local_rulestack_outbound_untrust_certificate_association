65d03886b67241da4ff9ea506b3ea39bcd02c8ad
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,8 @@ output "palo_alto_local_rulestack_outbound_untrust_certificate_associations" {
   description = "All palo_alto_local_rulestack_outbound_untrust_certificate_association resources"
   value       = azurerm_palo_alto_local_rulestack_outbound_untrust_certificate_association.palo_alto_local_rulestack_outbound_untrust_certificate_associations
 }
+output "palo_alto_local_rulestack_outbound_untrust_certificate_associations_certificate_id" {
+  description = "List of certificate_id values across all palo_alto_local_rulestack_outbound_untrust_certificate_associations"
+  value       = [for k, v in azurerm_palo_alto_local_rulestack_outbound_untrust_certificate_association.palo_alto_local_rulestack_outbound_untrust_certificate_associations : v.certificate_id]
+}
 
