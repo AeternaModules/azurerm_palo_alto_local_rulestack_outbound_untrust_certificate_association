94702df43c59af7f45da0108620a87e6f44446ef
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "palo_alto_local_rulestack_outbound_untrust_certificate_associations_id" {
+  description = "Map of id values across all palo_alto_local_rulestack_outbound_untrust_certificate_associations, keyed the same as var.palo_alto_local_rulestack_outbound_untrust_certificate_associations"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_outbound_untrust_certificate_association.palo_alto_local_rulestack_outbound_untrust_certificate_associations : k => v.id }
+}
 output "palo_alto_local_rulestack_outbound_untrust_certificate_associations_certificate_id" {
   description = "Map of certificate_id values across all palo_alto_local_rulestack_outbound_untrust_certificate_associations, keyed the same as var.palo_alto_local_rulestack_outbound_untrust_certificate_associations"
   value       = { for k, v in azurerm_palo_alto_local_rulestack_outbound_untrust_certificate_association.palo_alto_local_rulestack_outbound_untrust_certificate_associations : k => v.certificate_id }
